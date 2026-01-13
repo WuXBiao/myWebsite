64787fca46a7e3d1d8d0c432a972e7e40ea1a12b
BiaoSir <WuXBiao2021@163.com>
2026-01-13 17:26:01 +0800

    页面优化

--- my-blog/src/views/About.vue
+++ my-blog/src/views/About.vue
@@ -18,44 +18,44 @@
               </h1>
               
               <p class="text-gray-600 dark:text-gray-300">
-                Web 开发者 / 技术博主
+                个人开发者（golang，java， python，vue） / 技术分享人
               </p>
             </div>
             
             <div class="prose dark:prose-invert max-w-none">
               <h2>个人介绍</h2>
               <p>
-                你好！我是一名热爱技术的 Web 开发者，专注于前端和全栈开发。我喜欢探索新技术，解决复杂问题，并分享我的知识和经验。
+                你好！我是一名热爱技术的 Go 开发者，专注于服务端开发，大数据 SQL 开发。我喜欢探索新技术，解决复杂问题，并分享我的知识和经验。
               </p>
               
               <h2>技能</h2>
               <ul>
-                <li>前端开发：HTML, CSS, JavaScript, TypeScript</li>
-                <li>前端框架：Vue, React, Angular</li>
-                <li>后端开发：Node.js, Express, NestJS</li>
-                <li>数据库：MongoDB, MySQL, PostgreSQL</li>
-                <li>其他：Git, Docker, CI/CD, AWS</li>
+                <li>后端开发：Golang, Java, Python</li>
+                <li>前端开发：HTML, CSS, JavaScript</li>
+                <li>前端框架：Vue</li>
+                <li>数据库：MySQL, Redis, Doris, Hive</li>
+                <li>其他：Git, Docker, CI/CD</li>
               </ul>
               
               <h2>工作经历</h2>
               <p>
-                我有多年的 Web 开发经验，曾在多家公司担任前端开发工程师和全栈开发工程师。我参与过各种规模的项目，从小型网站到大型企业应用。
+                我有多年的 Web 开发经验，曾在多家公司担任 Golang 开发工程师和SQL工程师。我参与过各种规模的项目，从小型系统到大型企业应用。
               </p>
               
               <h2>教育背景</h2>
               <p>
-                计算机科学与技术专业学士学位
+                软件工程专业学士学位
               </p>
               
               <h2>联系方式</h2>
               <p>
                 邮箱：1472396277@qq.com<br>
-                GitHub：<a href="https://github.com/WuXBiao/" target="_blank">github.com/yourusername</a><br>
+                GitHub：<a href="https://github.com/WuXBiao/" target="_blank">github.com/WuXBiao/</a><br>
               </p>
               
               <h2>关于本站</h2>
               <p>
-                这个博客是我用来分享技术文章、教程和个人思考的平台。主要内容包括前端开发、全栈技术、性能优化等。
+                这个博客是我用来分享技术文章、教程和个人思考的平台。主要内容包括开发知识、部署技能、性能优化等。
               </p>
             </div>
           </div>
